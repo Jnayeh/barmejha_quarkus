6071e4a6c7b5c5aa48db5dec512b894f8bd7722c
Mourad Slim Jnayeh <mouradslim44@gmail.com>
2025-03-09 19:12:38 +0100

    initial commit

---
+++ src/test/java/org/barmejha/resources/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package org.barmejha.resources;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}

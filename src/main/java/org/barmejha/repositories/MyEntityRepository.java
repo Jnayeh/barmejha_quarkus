707893c526109a6e20fb05b0bc9514e71b5a8201
MouradSlimJnayeh <mourad.jnayeh@keyrus.com>
2025-03-09 19:25:03 +0100

    removes repo injection

--- src/main/java/org/barmejha/repositories/MyEntityRepository.java
+++ src/main/java/org/barmejha/repositories/MyEntityRepository.java
@@ -1,9 +1,7 @@
 package org.barmejha.repositories;
 
 import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
-import jakarta.enterprise.context.ApplicationScoped;
 import org.barmejha.domain.entities.MyEntity;
 
-@ApplicationScoped
 public interface MyEntityRepository extends PanacheEntityResource<MyEntity, Long> {
 }

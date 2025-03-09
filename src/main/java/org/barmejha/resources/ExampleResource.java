e272208c089a843e32e7333b2f1a8df75bee8dcb
Mourad Slim Jnayeh <mouradslim44@gmail.com>
2025-03-09 19:25:03 +0100

    removes repo injection

--- src/main/java/org/barmejha/resources/ExampleResource.java
+++ src/main/java/org/barmejha/resources/ExampleResource.java
@@ -1,25 +1,29 @@
 package org.barmejha.resources;
 
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import org.barmejha.repositories.MyEntityRepository;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @Path("/hello")
 @ApplicationScoped
-@Produces(MediaType.APPLICATION_JSON)
 public class ExampleResource {
 
-  @Inject
-  MyEntityRepository myEntityRepository;
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String hello() {
+    return "Hello from Quarkus REST";
+  }
 
   @GET
-  public HashMap<String, String> hello() {
+  @Path("/json")
+  @Produces(MediaType.APPLICATION_JSON)
+
+  public Map<String, String> helloJson() {
     var h = new HashMap<String, String>();
     h.put("hello", "Hello from Quarkus REST");
     return h;

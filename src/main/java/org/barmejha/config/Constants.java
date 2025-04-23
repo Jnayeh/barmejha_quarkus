d7b9212abe51f622c38c60012aa7429e5f005dce
Mourad Slim Jnayeh <mouradslim44@gmail.com>
2025-04-23 13:57:01 +0100

    adds config files

---
+++ src/main/java/org/barmejha/config/Constants.java
@@ -0,0 +1,10 @@
+package org.barmejha.config;
+
+public class Constants {
+  public static final String X_LANG = "x-lang";
+  public static final String X_MSISDN = "x-msisdn";
+  public static final String X_USER = "x-user";
+  public static final String X_VERSION = "x-version";
+  public static final String X_TENANT = "x-tenant";
+  public static final String X_API_VERSION = "x-api-version";
+}

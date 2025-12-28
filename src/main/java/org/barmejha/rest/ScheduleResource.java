39de1cae3eafea38d9ca13098a1188ee3a3db8a8
Mourad Slim Jnayeh <mouradslim44@gmail.com>
2025-12-28 12:08:46 +0100

    fix: removes unneeded resource

--- src/main/java/org/barmejha/rest/ScheduleResource.java
+++ src/main/java/org/barmejha/rest/ScheduleResource.java
@@ -2,11 +2,7 @@ package org.barmejha.rest;
 
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.barmejha.domain.dtos.ScheduleDTO;
 import org.barmejha.domain.entities.Schedule;

6a4fe96d39852a1849d017dc6a19d9786f2ed96d
Mourad Slim Jnayeh <mouradslim44@gmail.com>
2025-12-28 11:59:07 +0100

    fix: changes object mapper conversion

--- src/main/java/org/barmejha/domain/entities/schedule/LocalDateConverter.java
+++ src/main/java/org/barmejha/domain/entities/schedule/LocalDateConverter.java
@@ -3,6 +3,7 @@ package org.barmejha.domain.entities.schedule;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.quarkus.logging.Log;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
@@ -15,17 +16,18 @@ import java.util.Set;
 public class LocalDateConverter implements AttributeConverter<Set<LocalDate>, String> {
   @Override
   public String convertToDatabaseColumn(Set<LocalDate> days) {
+    if (days == null) return null;
     try {
-      return new ObjectMapper().writeValueAsString(days);
+      return new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(days);
     } catch (JsonProcessingException e) {
-      throw new InternalServerErrorException("Error processing the date json", e);
+      throw new InternalServerErrorException("Error processing the date json: ", e);
     }
   }
 
   @Override
   public Set<LocalDate> convertToEntityAttribute(String json) {
     try {
-      return new ObjectMapper().readValue(json, new TypeReference<>() {
+      return new ObjectMapper().registerModule(new JavaTimeModule()).readValue(json, new TypeReference<>() {
       });
     } catch (JsonProcessingException e) {
       Log.error("Error processing the date json", e);
